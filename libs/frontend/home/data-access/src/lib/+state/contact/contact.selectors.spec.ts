d3471665edc304b368a4d5f0ed42ee9cd312ebfe
Michał <michals1099@gmail.com>
2023-05-15 15:44:00 +0200

    CHAT-11 apply prettier

--- libs/frontend/home/data-access/src/lib/+state/contact/contact.selectors.spec.ts
+++ libs/frontend/home/data-access/src/lib/+state/contact/contact.selectors.spec.ts
@@ -4,7 +4,7 @@ import { selectContactState } from './contact.selectors';
 describe('Contact Selectors', () => {
   it('should select the feature state', () => {
     const result = selectContactState({
-      [fromContact.contactFeatureKey]: {}
+      [fromContact.contactFeatureKey]: {},
     });
 
     expect(result).toEqual({});
